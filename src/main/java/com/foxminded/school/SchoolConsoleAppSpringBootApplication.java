26fa825db35ef8b6c20c30bdcb011cf90d559e32
Petro Kobevko <kobevkopi@gmail.com>
2023-01-19 22:33:13 +0100

    Initial commit

---
+++ src/main/java/com/foxminded/school/SchoolConsoleAppSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.foxminded.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolConsoleAppSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SchoolConsoleAppSpringBootApplication.class, args);
+    }
+}
---
+++ src/test/java/com/foxminded/school/SchoolConsoleAppSpringBootApplicationTests.java
@@ -0,0 +1,11 @@
+package com.foxminded.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolConsoleAppSpringBootApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
