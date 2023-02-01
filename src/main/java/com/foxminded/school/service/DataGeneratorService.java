b6d4f81824f2c1197228a205bd08bfb0fe7c0de5
Petro Kobevko <kobevkopi@gmail.com>
2023-02-01 02:03:41 +0100

    Added a check for empty tables when generating data

--- src/main/java/com/foxminded/school/service/DataGeneratorService.java
+++ src/main/java/com/foxminded/school/service/DataGeneratorService.java
@@ -5,13 +5,15 @@ import com.foxminded.school.entity.Group;
 import com.foxminded.school.entity.Student;
 import com.foxminded.school.util.data.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
 
 @Service
-public class DataGeneratorService {
+public class DataGeneratorService implements ApplicationRunner {
     private final Data data = new Data();
 
     private CourseService courseService;
@@ -36,5 +38,12 @@ public class DataGeneratorService {
         Map<Student, List<Course>> studentsCourses = data.getStudentsCourses(students, courses);
         studentService.saveAll(students);
         studentService.assignToCourses(studentsCourses);
+    }
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        if (courseService.getAll().isEmpty() && groupService.getAll().isEmpty() && studentService.getAll().isEmpty()) {
+            insertTestDataInDatabase();
+        }
     }
 }
